5e4a2f376ee8b170a66716b16b06e1b25360e0b7
VictorHugo24 <vh33079@gmail.com>
2024-11-08 10:30:06 -0600

    Examen 75%

---
+++ Rodriguez_VictorE1/src/rodriguez_victore1/Rodriguez_VictorE1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rodriguez_victore1;
+
+/**
+ *
+ * @author Victor
+ */
+public class Rodriguez_VictorE1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
